7a31719e65729db3c2e71fe777e8591ef4e88077
willard-yuan <yongyuanstu@gmail.com>
2014-03-16 09:47:46 +0800

    Update ch01_fig1-3_contour.py

--- pcv-notebook/ch01/ch01_fig1-3_contour.py
+++ pcv-notebook/ch01/ch01_fig1-3_contour.py
@@ -5,11 +5,12 @@ from pylab import *
 im = array(Image.open('../data/empire.jpg').convert('L'))  # 打开图像，并转成灰度图像
 
 figure()
+subplot(121)
 gray()
 contour(im, origin='image')
 axis('equal')
 axis('off')
 
-figure()
+subplot(122)
 hist(im.flatten(), 128)
 show()
